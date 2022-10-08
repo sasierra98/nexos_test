da6f84f75ffd79786eff14902ac2dfdd1cf22d6f
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-10-08 00:58:57 -0500

    User model added

---
+++ base_model/models/User.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+
+
+class User(AbstractUser, PermissionsMixin):
+    gln_user = models.PositiveIntegerField(
+        verbose_name=_('GLN client'),
+        unique=True,
+        primary_key=True
+    )
+
+    phone = models.CharField(
+        verbose_name=_('Phone'),
+        max_length=50,
+        blank=True,
+        null=True
+    )
+
+    address = models.CharField(
+        verbose_name=_('Address'),
+        max_length=255,
+        blank=True,
+        null=True
+    )
+
